40853a18fcd77e3c1f7c67273035934ffe6ced6b
werbethe <werbethe@medware.com.br>
2020-12-12 22:39:27 -0300

    adicionado componente home e bookmarks

--- testeclima/src/app/app-routing.module.ts
+++ testeclima/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
+import { BookmarksPage } from './pages/bookmarks/bookmarks.page';
+import { HomePage } from './pages/home/home.page';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", component: HomePage },
+  { path: "bookmarks", component: BookmarksPage }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
