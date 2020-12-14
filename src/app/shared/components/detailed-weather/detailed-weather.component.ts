128219a287c778814d6bacab990021dd5da90dcc
werbethe <werbethe@medware.com.br>
2020-12-13 22:14:19 -0300

    adicionado component detailed

---
+++ src/app/shared/components/detailed-weather/detailed-weather.component.ts
@@ -0,0 +1,18 @@
+import { ChangeDetectionStrategy, Component, Input, OnInit } from '@angular/core';
+
+import { Weather } from './../../models/weather.model';
+
+@Component({
+    selector: 'app-detailed-weather',
+    templateUrl: './detailed-weather.component.html',
+    styleUrls: ['./detailed-weather.component.scss'],
+    changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class DetailedWeatherComponent {
+    @Input() weather: Weather;
+
+    get weatherIcon(): string {
+        return `http://openweathermap.org/img/wn/${this.weather.icon}@2x.png`;
+    }
+
+}
